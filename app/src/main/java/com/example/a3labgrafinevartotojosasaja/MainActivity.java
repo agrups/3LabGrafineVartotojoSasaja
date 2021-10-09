f7928257c9e2ee0f21179ab1dd8fc3982d3e82e0
Greta Klimavičiūtė <greta.klimaviciute@stud.vgtu.lt>
2021-10-09 19:07:25 +0300

    added some functionalities

--- app/src/main/java/com/example/a3labgrafinevartotojosasaja/MainActivity.java
+++ app/src/main/java/com/example/a3labgrafinevartotojosasaja/MainActivity.java
@@ -1,23 +1,33 @@
-package com.example.a3labgrafinevartotojosasaja;
+ package com.example.a3labgrafinevartotojosasaja;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    EditText name;
     AutoCompleteTextView faculty;
     RatingBar rating;
     Spinner day;
     TimePicker time;
     Switch register;
+    Button save;
+    String text;
+    String daySelected;
 
     private static final String[] FACULTIES = new String[] {
             "Antano Gustaičio aviacijos institutas", "Aplinkos inžinerijos fakultetas", "Architektūros fakultetas", "Elektronikos fakultetas",
@@ -26,20 +36,53 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (EditText) findViewById(R.id.pavadinimasText);
+
         faculty = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
 
         ArrayAdapter<String> facultyAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, FACULTIES);
         faculty.setThreshold(1);
         faculty.setAdapter(facultyAdapter);
 
-//        day = (Spinner)findViewById(R.id.spinner2);
-//        day.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-//
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.days_of_week, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        day.setAdapter(adapter);
+        rating = (RatingBar) findViewById(R.id.ratingBar);
+
+        day = (Spinner)findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.days_of_week, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        day.setAdapter(adapter);
+        day.setOnItemSelectedListener(this);
+
+        time = (TimePicker) findViewById(R.id.timePicker);
+
+        register = (Switch) findViewById(R.id.switch1);
+
+        save = (Button) findViewById(R.id.saugotiButton);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                text = "Pavadinimas: " + name.getText().toString() + "\n" +
+                        "Fakultetas: " + faculty.getText().toString() + "\n" +
+                        "Sedėtingumas: " + rating.getRating() + "\n" +
+                        "Diena: " + daySelected + "\n";
+                Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
+            }
+        });
     }
-}
+
+     @Override
+     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+         daySelected = parent.getItemAtPosition(position).toString();
+     }
+
+     @Override
+     public void onNothingSelected(AdapterView<?> parent) {
+
+     }
+ }
